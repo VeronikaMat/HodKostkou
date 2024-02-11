a19ecf565e22ec8e35cf38db924e7e4e3092930f
Veronika Matoušková <andrlovaver@gmail.com>
2024-02-11 16:13:04 +0100

    upravila jsem JS

--- script.js
+++ script.js
@@ -5,4 +5,4 @@ let teplota= Number (prompt("Zadej teplotu v jednotkách °F:"))
 
 const stupneC= (5/9 * (teplota-32))
 
-document.body.innerHTML = Math.round(stupneC)
+document.body.innerHTML = Math.round(+ stupneC + "°C" )
